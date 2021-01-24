081e563f2c315b7772a476e9f6feffd53802592b
Mark Dekker <maakaas@gmail.com>
2021-01-24 16:30:28 +0100

    Handle arrow keys

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang='typescript'>
   import {onMount} from 'svelte';
-import { currentSlide, totalSlides } from './state';
+  import { currentSlide, totalSlides } from './state';
   import './styling/main.css';
   
   import TitlePage from './templates/TitlePage.svelte'
@@ -24,7 +24,24 @@ import { currentSlide, totalSlides } from './state';
       return n
     })
   }
+
+  const handleKeydown = (event) => {
+    const key = event.key;
+    console.log(key)
+    switch (key) {
+      case "ArrowRight":
+        nextSlide()
+        break;
+      case "ArrowLeft":
+        prevSlide()
+        break;
+      default:
+        break;
+    }
+  }
 </script>
+
+<svelte:window on:keydown={handleKeydown}/>
 
 <div class="App">
   <div class="fixed h-full w-28 left-0 top-0 z-50 
@@ -40,6 +57,6 @@ import { currentSlide, totalSlides } from './state';
   <TitlePage
     title="This is the second slide"
     subtitle="How does clicking through work."
-    backgroundImage={"https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2767&q=80"}/>
+    backgroundImage={"https://images.unsplash.com/photo-1611444275099-d39965256256?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80"}/>
 </div>
   
